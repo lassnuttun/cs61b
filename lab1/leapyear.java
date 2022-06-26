8e22000153e638afe35a177f3fbc6dfb0a334b70
lassnuttun <3059167604@qq.com>
2022-06-26 16:20:57 +0800

    add the function isLeapYear

---
+++ lab1/leapyear.java
@@ -0,0 +1,18 @@
+package lab1;
+
+public class leapyear {
+    public static boolean isLeapYear(int year) {
+        if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) {
+            return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        if (isLeapYear(Integer.parseInt(args[1]))) {
+            System.out.println(Integer.parseInt(args[1]) + " is a leap year.");
+        } else {
+            System.out.println(Integer.parseInt(args[1]) + " is not a leap year.");
+        }
+    }
+}
