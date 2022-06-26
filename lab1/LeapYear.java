ae1da042bf9687e32c94de44b25277b32b09f6b5
lassnuttun <3059167604@qq.com>
2022-06-26 17:13:56 +0800

    rename the class

---
+++ lab1/LeapYear.java
@@ -0,0 +1,16 @@
+public class LeapYear {
+    public static boolean isLeapYear(int year) {
+        if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) {
+            return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        if (isLeapYear(Integer.parseInt(args[0]))) {
+            System.out.println(Integer.parseInt(args[0]) + " is a leap year.");
+        } else {
+            System.out.println(Integer.parseInt(args[0]) + " is not a leap year.");
+        }
+    }
+}
